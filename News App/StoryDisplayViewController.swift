f7f5437b3d4dc8ff71b7027b78bb3c9697291ddb
phgupta <phgupta@ucdavis.edu>
2017-10-27 12:48:37 -0700

    Design changes

--- News App/StoryDisplayViewController.swift
+++ News App/StoryDisplayViewController.swift
@@ -9,41 +9,58 @@
 import UIKit
 
 class StoryDisplayViewController: UIViewController {
-
+    
     // Global Variables
     var activeRow = 0
     var articles: [ArticleObject]? = []
     var username = "mystring"
-
     
-    // Outlets
-    @IBOutlet weak var articleTitle: UITextView!
-    @IBOutlet weak var textbody: UITextView!
+    //Outlets
     
+    @IBAction func backbuttonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "backButtonArticleVC", sender: nil)
+    }
+    
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var articleTitle: UILabel!
+    @IBOutlet weak var textbody: UILabel!
+    @IBOutlet weak var articleImage: UIImageView!
     
     // Default functions
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        authorLabel.text = articles![activeRow].author
         articleTitle.text = articles![activeRow].title
         textbody.text = articles![activeRow].body
+        articleImage.downloadImage(from: (articles![activeRow].imageUrl!))
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if (segue.identifier == "backButtonArticleVC") {
+            
+            let articleDisplayViewController = segue.destination as! ArticleTableViewController
+        }
     }
-    */
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
+
+
